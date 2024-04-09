458f3c6ae57f602c884415041215b26eff887ad5
Brian Jones <brian.edward.jones@gmail.com>
2024-04-09 14:22:35 -0400

    app.vue and header.vue update

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,14 +5,21 @@ import VueCarousel from 'vue-carousel';
 Vue.use(VueCarousel);
 
 /*added Vue and Vue Carousel after running npm install vue-carousel*/
-
+I added the hidden-link class to router and used css to hide the nav-bar for now
 
 <template>
   <div id="capstone-app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link class="hidden-link" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link class="hidden-link" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<style>
+.hidden-link {
+  display: none;
+}
+
+</style>
