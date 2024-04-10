b617059e3ab621749288d87ba8bbda03420c1d88
Brian Jones <brian.edward.jones@gmail.com>
2024-04-09 22:18:24 -0400

    front end updates

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -7,7 +7,6 @@
     <nav class="header-nav">
         <ul class="nav-list">
             <li class="nav-item"><router-link to="/" class="nav-link">Home</router-link></li>
-            <li class="nav-item"><router-link to="/about" class="nav-link">About</router-link></li>
             <li class="nav-item"><router-link to="/breweries" class="nav-link">Breweries</router-link></li>
             <li class="nav-item"><router-link to="/beers" class="nav-link">Beers</router-link></li>
             <li class="nav-item"><router-link to="/contact" class="nav-link">Contact</router-link></li>
