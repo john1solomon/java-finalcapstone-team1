e8ca3fa902f10af8fbbe87520911dbb6dfc8b87a
John Solomon <johntsolomon@gmail.com>
2024-04-11 09:04:21 -0400

    Updates for the brewer finder database tables and data

--- java/database/create.sh
+++ java/database/create.sh
@@ -5,5 +5,8 @@ DATABASE=final_capstone
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/data_users.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/data_brewer.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/data_brewery.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/data_brewery_beer.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,9 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, brewer, brewery, brewery_beer;
+--DROP TABLE IF EXISTS brewer, brewery, brewery_beer;
+--DROP TABLE IF EXISTS brewery, brewery_beer;
+--DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +11,45 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE brewer (
+ 	username varchar(50) NOT NULL UNIQUE,
+ 	brewer_name varchar(50) NOT NULL UNIQUE,
+ 	CONSTRAINT PK_username PRIMARY KEY (username),
+	CONSTRAINT FK_username FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE TABLE brewery (
+-- 	brewery_id SERIAL,
+	brewery_id int NOT NULL,
+	brewery_name varchar(100) NOT NULL UNIQUE,
+	brewer_name varchar(50) NOT NULL,
+	contact_information varchar(100),
+	street_address varchar(100) NOT NULL,
+	city varchar(50) NOT NULL,
+	state_code char(2) NOT NULL,
+	postal_code varchar(10) NOT NULL,
+	history varchar(100),
+	active boolean NOT NULL,
+	days_hours_of_operation varchar(100),
+	CONSTRAINT PK_brewery PRIMARY KEY (brewery_id),
+	CONSTRAINT FK_brewer_name FOREIGN KEY (brewer_name) REFERENCES brewer (brewer_name)
+);
+
+CREATE TABLE brewery_beer (
+--	brewery_beer_id SERIAL,
+	brewery_beer_id int NOT NULL,
+	brewery_id int NOT NULL,
+	beer_name varchar(100) NOT NULL,
+	beer_description varchar(100),
+	beer_type varchar(50) NOT NULL,
+	abv decimal(4,1) NOT NULL,
+	num_ratings int NOT NULL CONSTRAINT DF_num_ratings DEFAULT(0),
+	avg_rating decimal(4,2) CONSTRAINT DF_avg_ratings DEFAULT(0),
+	last_active date NOT NULL,
+	CONSTRAINT PK_brewery_beer PRIMARY KEY (brewery_beer_id),
+	CONSTRAINT FK_brewery_id FOREIGN KEY (brewery_id) REFERENCES brewery (brewery_id)
 );
 
 COMMIT TRANSACTION;
