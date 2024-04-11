4803e3cbcabde9f87c135b1a4ade835996d66bc5
John Solomon <johntsolomon@gmail.com>
2024-04-11 09:19:28 -0400

    Initial brewery finder DAO and controller files

--- java/src/main/java/com/techelevator/controller/BreweryController.java
+++ java/src/main/java/com/techelevator/controller/BreweryController.java
@@ -1,2 +1,45 @@
-package com.techelevator.controller;public class BreweryController {
+package com.techelevator.controller;
+
+import com.techelevator.dao.BreweryDao;
+import com.techelevator.model.Beer;
+import com.techelevator.model.Brewery;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BreweryController {
+    private BreweryDao breweryDao;
+
+    public BreweryController(BreweryDao breweryDao) {
+        this.breweryDao = breweryDao;
+    }
+
+    @RequestMapping(path = "/brewery", method = RequestMethod.GET)
+    public List<Brewery> getBreweries() {
+        return breweryDao.getBreweries();
+    }
+
+    @RequestMapping(path = "/brewery/{breweryId}", method = RequestMethod.GET)
+    public Brewery getBreweryById(@PathVariable int breweryId) {
+        return breweryDao.getBreweryById(breweryId);
+    }
+
+    @RequestMapping(path = "/beer", method = RequestMethod.GET)
+    public List<Beer> getBeers() {
+        return breweryDao.getBeers();
+    }
+
+    @RequestMapping(path = "/brewery/{breweryId}/beer", method = RequestMethod.GET)
+    public List<Beer> getBeersForBrewery(@PathVariable int breweryId) {
+        return breweryDao.getBeersForBrewery(breweryId);
+    }
+
+    @RequestMapping(path = "/beer/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable int beerId) {
+        return breweryDao.getBeerById(beerId);
+    }
 }
--- java/src/main/java/com/techelevator/dao/BreweryDao.java
+++ java/src/main/java/com/techelevator/dao/BreweryDao.java
@@ -1,2 +1,13 @@
-package com.techelevator.dao;public interface BreweryDao {
+package com.techelevator.dao;
+
+import com.techelevator.model.Beer;
+import com.techelevator.model.Brewery;
+
+import java.util.List;
+public interface BreweryDao {
+    List<Brewery> getBreweries();
+    Brewery getBreweryById(int breweryId);
+    List<Beer> getBeers();
+    List<Beer> getBeersForBrewery(int breweryId);
+    Beer getBeerById(int beerId);
 }
